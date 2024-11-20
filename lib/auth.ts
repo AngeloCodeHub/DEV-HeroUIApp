a6985f36d2003c7eee68ae4c8b6a350297863abc
Angelo <a0920217700@gmail.com>
2024-11-21 02:10:34 +0800

    eslint降級，修復eslint報錯，dependencies升級

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,8 @@
-
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    // pages: {
-    //     signIn: "/login",
-    //   },
-    providers: [],
-})
-
-
+  // pages: {
+  //     signIn: "/login",
+  //   },
+  providers: [],
+});
