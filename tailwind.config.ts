420ad4ba2490effb7c0520af0f968849e8597abe
Angelo <a0920217700@gmail.com>
2024-12-18 21:08:05 +0800

    rebuild

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/theme";
+
+export default {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+      },
+    },
+  },
+  plugins: [nextui()],
+  darkMode: "class",
+} satisfies Config;
