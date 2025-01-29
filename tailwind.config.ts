6f728b39779857a5bb165c8b17b3f186c1896c51
Angelo <a0920217700@gmail.com>
2025-01-29 14:30:27 +0800

    更新依賴庫，將 NextUI 替換為 HeroUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from "tailwindcss";
-import { nextui } from "@nextui-org/theme";
+
+import { heroui } from "@heroui/theme";
 
 export default {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -15,6 +16,6 @@ export default {
       },
     },
   },
-  plugins: [nextui()],
+  plugins: [heroui()],
   darkMode: "class",
 } satisfies Config;
