9315bca9798b759bcdd0ca3398a4d4396bfaa3e0
Angelo <a0920217700@gmail.com>
2025-04-19 14:44:40 +0800

    更新 next.config.ts，設定 basePath 為 '/app'

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig: import("next").NextConfig = {};
+const nextConfig: import("next").NextConfig = {
+  basePath: '/app'
+};
 
 export default nextConfig;
